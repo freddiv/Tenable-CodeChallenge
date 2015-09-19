847d74f56c52b21537142623a2caac00b8bd8ffe
frederick Valone <drag0nsweb@netscape.net>
2015-09-19 19:51:40 -0400

    update readme and add maxDiff.js

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Tenable-CodeChallenge
-Code Challenge for Tenable
+# Supporting code for Tenable coding challenge
+## tests.js
+Code in tests.js was written to support verifying answers in multiple choice questions and to be used in actual code challenges
+
+### maxDiff()
+This function is a customized version of themaxDifference function in question 6 of the code challenge
+
+It takes form input of a comma or space delimited list. It converts the array items to integers when possiple if the parseInt function returns NaN on the array item that item is removed from the array.
+
+If the first item in the array is not equal to the length of the remaining items in the array this is shown in the output.
+ 
+
+## maxDiff.js
+This is the original code that I submitted for question 6
+
+I could not get all test cases to succeed and the fact that I had no access to the values being passed in or the details for the failed test cases I was not able to effectively debug.
+
+Similiarly, no output was displayed to show what the function was returning 
+
+## Running the code
+Download the repository into a directory on a web server's root.
+navigate to the directory and load index.html example: http://localhost/tenable/index.html
+follow the on screen directions to run the maxDiff function.
--- tests.js
+++ tests.js
@@ -1,26 +1,73 @@
 
 $( document ).ready(function() {
-    console.log( "ready!" );
     $( "#maxDiff" ).click(function( event ) {
     	  event.preventDefault();
     	  var arrVal = $('input#maxDiffVals').val(),
     	  a;
     	  
-    	  console.log(arrVal.indexOf(',')); 
-    	  if (arrVal.indexOf(',') > -1){
-    		  
+    	 // check input for a comma to determine list format for parsing into an array
+    	  if (arrVal.indexOf(',') > -1){	  
     		a = arrVal.split(',');  
     	  }
     	  else{
     		  a = arrVal.split(' ');    
     	  }
     	  difference = maxDiff(a);
-    	  console.log(arrVal);
-    	  console.log(a);
-    	  console.log(difference);
+    	  $('div#maxDiffOut').find('p').append("maxDifference returned " + difference);
+    	  
     	});
     
 });
+
+function getSmallest(numbers) {
+	var numberArray = numbers.split(','),
+	    index,
+	    len,
+	    minNum;
+	    for (index = 0, len = numberArray.length; index < len; ++index ){
+	      var parsedNum = parseInt(numberArray[index]);
+			if(index === 0){
+				minNum = parsedNum;
+			}
+			else if(minNum > parsedNum){
+				minNum = parsedNum;
+			}  
+	    }
+	    return minNum;
+	}
+
+function capitalize(string) {
+	 var stringArr = string.split(""),
+	     vowels = ["a","e","i","o","u","y"],
+	     newString = "",
+	     index,
+	     len;
+	     for (index = 0, len = stringArr.length; index < len; ++index ){
+		      var item = stringArr[index],
+	              converted;
+		   
+				if(vowels.indexOf(item) >-1){
+					converted = item.toUpperCase();
+	                newString += converted;
+				}
+				else {
+					newString += item;
+				}  
+		    }
+	     
+		    return newString;
+	}
+
+/*
+ * Complete the function below.
+ * this function works in every case were an array of integers is passed in to it on my local server (not using node and using form input)
+ * I have no access to the test cases that are failing or even the error or output so I cannot debug effectively
+ * see code at https://github.com/freddiv/Tenable-CodeChallenge for a custom implementation 
+ * custom implementation works whether it is numeric or string values
+ * it also handles errors such as an alpha value being passed in the array 
+ * and the first value in the input string not correctly indicating the array length
+ * input can be space or comma delimited
+ */
 
 function maxDiff(a) {
 	 var arrInput = a || [8,3,7,3,8,20,33,1,8], //default when a null value is passed
@@ -35,22 +82,12 @@ function maxDiff(a) {
 	 	 minNumber = parseInt(Math.min.apply(null, finalArray)), // get the lowest value in the array created in line above
 	 	 numDiff = (maxNumber - minNumber); // subtract the low from the high value for the max difference
 	 		
-	 console.log('arr', arr);
-	 console.log('inputLen', inputLen);
 	 lineOut += "input length is " + inputLen + "<br />";
 	 lineOut += "maxNumber is " + maxNumber + "<br />";
 	 lineOut += "maxNumber was found at array[" + (parseInt(finalArrLen)) + "] <br />";
 	 lineOut += "the length of array values found before the maxNumber is " + finalArray.length + " <br />";
 	 lineOut += "the minNumber found in that array is " + minNumber + " <br />";
 	 
-	 console.log('newArr', newArr);
-	 console.log('maxNumber', maxNumber);
-	 console.log('minNumber', minNumber);
-	 console.log('finalArrLen', finalArrLen);
-	 console.log('finalArray', finalArray);
-	 console.log('numDiff', numDiff);
-	 console.log(numDiff == NaN);
-	 console.log('returnVal',returnVal);
 	 if(!isNaN(numDiff) && inputLen === newArr.length ) {
 		  console.log('returnVal',returnVal);
 		  lineOut += "the value passed for the length of the array matches the parsed array length and difference between min and max values is  " + numDiff + " <br />";
@@ -95,3 +132,44 @@ function numericArray(numArray) {
 	
 	  return returnArray;
 	}
+(function(){
+	var a = 100;
+	outputStr = "";
+	( function(){
+		if(!a) {
+			var a = 300;
+		}
+		else{
+			a+= 50;
+		}	
+		outputStr += a + '<br />';
+
+	} )();
+	outputStr += a + '<br />';
+	$('div#hoist').find('p').html('').append(outputStr);
+} )();
+
+function updateState(object){
+	state = "Virginia";
+	object.city = "Alexandria";
+	
+	return state;
+}
+var newState = "Alaska",
+ouputStr = "",
+obj = {
+		city:'Columbia',
+		state: 'Maryland',
+		counter: 1
+};
+
+
+newState = updateState(obj);
+
+ouputStr += obj.city + '<br />';
+ouputStr += obj.state + '<br />';
+ouputStr += newState + '<br />';
+
+$('div#object').find('p').html('').append(ouputStr);
+
+
